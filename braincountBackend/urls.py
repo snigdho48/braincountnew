7b54b0a20caf12053c88d9bff653dd6a2a6e19b4
snigdho48 <www.atik48@gmail.com>
2025-04-16 12:12:06 +0600

    add patch

--- braincountBackend/urls.py
+++ braincountBackend/urls.py
@@ -23,9 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
